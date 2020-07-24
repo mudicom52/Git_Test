2472d80fdbef249f4c038e7deaa4f072c8b36791
mudicom52 <68753048+mudicom52@users.noreply.github.com>
2020-07-24 22:33:01 +0300

    Update TestClass.java

--- src/TestClass.java
+++ src/TestClass.java
@@ -3,6 +3,7 @@ import java.util.Collections;
 public class TestClass {
     public static void main(String[] args) {
         System.out.println("Hi from Git");
+        System.out.println("Hi from GitHub");
     }
 
 }
